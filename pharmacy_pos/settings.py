11f94719834b09dc7e0756ba91da6bb7c67599e0
Mamadou Moustapha Diallo <moustaphaotf@gmail.com>
2025-11-11 13:52:28 +0000

    chore: update also settings to read the DEBUG var from env variables

--- pharmacy_pos/settings.py
+++ pharmacy_pos/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-029u0k%@0*%fafq4u_l-i@5(&cp5)o1ro23^x&c@nshrd0sr#4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
