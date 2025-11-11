6f52348446d0a806c9b05d30913f43e4f9214260
Mamadou Moustapha Diallo <moustaphaotf@gmail.com>
2025-11-11 13:32:50 +0000

    chore: set an env var to hold the database url and update settings

--- pharmacy_pos/settings.py
+++ pharmacy_pos/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,10 +90,10 @@ WSGI_APPLICATION = 'pharmacy_pos.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db3.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+    )
 }
 
 
